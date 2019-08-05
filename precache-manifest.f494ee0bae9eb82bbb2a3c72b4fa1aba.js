e0263eea27e62005eef50bf34ed4800062e24638
Daniel Williams <daniellanierwilliams@gmail.com>
2019-08-05 09:51:26 -0500

    Updates

---
+++ precache-manifest.f494ee0bae9eb82bbb2a3c72b4fa1aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7101e601411731e67614d06f06831dfb",
+    "url": "/LooneyTunes2/index.html"
+  },
+  {
+    "revision": "bb6c51920f171931c117",
+    "url": "/LooneyTunes2/static/css/main.3d5f3112.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/LooneyTunes2/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bb6c51920f171931c117",
+    "url": "/LooneyTunes2/static/js/main.0b18b16a.chunk.js"
+  },
+  {
+    "revision": "fb184e38e401d2d8424f",
+    "url": "/LooneyTunes2/static/js/runtime~main.8cf8ff9e.js"
+  }
+]);
