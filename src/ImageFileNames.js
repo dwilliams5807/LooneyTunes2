a9b150685922fd77cf0f8f531000572866bd53f4
Daniel Williams <daniellanierwilliams@gmail.com>
2019-08-05 09:48:07 -0500

    final commit

---
+++ src/ImageFileNames.js
@@ -0,0 +1,3 @@
+let ImageFileNames = ["porky.jpg", "bugs1.png", "bugs2.jpg", "daffy.jpg", "elmer.jpg", "frogguy.png", "marvin.jpg", "roadrunner.jpg", "rockyandmugsy.jpg", "speedy.jpg", "sylvester.jpg", "tazz.jpg", "tweety1.jpg", "tweety2.jpg", "wiley.jpg", "yosemeti.jpg"];
+
+export default ImageFileNames;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>React Clicky Game</span>
+  </footer>
+);
+
+export default Footer;
